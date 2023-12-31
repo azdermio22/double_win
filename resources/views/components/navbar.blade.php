a73acda96060302d50087501c4b4737723d5b448
christian conti <christian.conti123@gmail.com>
2023-12-31 15:53:55 +0100

    nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,11 @@
 <nav class="container-fluid">
 <div class="row">
     <div class="col-4">
-        <h1>double win</h1>
+        @if (Route::currentRouteName() != "home")
+        <h1>{{Route::currentRouteName()}}</h1> 
+        @else      
+        <h1>double <span class="w">v</span>vin</h1>
+        @endif
     </div>
     <div class="col-4 d-flex justify-content-evenly align-items-center">
         <div class="position-relative d-flex justify-content-center">
@@ -14,16 +18,25 @@
         </div>
     </div>
     <div class="col-4 d-flex justify-content-evenly align-items-center">
-        @guest          
-        <a class="nav_link" href="{{route('login')}}"><i class="bi bi-box-arrow-in-right"></i></a>
-        <a class="nav_link" href="{{route('register')}}">register</a>
+        @guest  
+        <div class="position-relative d-flex justify-content-center">
+            <a class="login_link nav_link fs-3 d-flex align-items-center" href="{{route('login')}}"><i class="bi bi-box-arrow-in-right"></i><div class="login_container position-relative d-flex align-items-center overflow-hidden"><div class="login_link2 fs-5">login</div></div></a>
+            <div class="link_hover"></div>
+        </div>
+        <div class="position-relative d-flex justify-content-center">
+            <a class="register_link nav_link fs-3 d-flex align-items-center" href="{{route('register')}}"><i class="bi bi-person-fill-add"></i><div class="register-container position-relative d-flex align-items-center overflow-hidden"><div class="register_link2 fs-5">register</div></div></a>
+            <div class="link_hover"></div>
+        </div>        
         @endguest
         @auth
         <form method="POST" action="{{route('logout')}}">
         @csrf
-        <button type="submit">logout</button>
+        <div class="position-relative d-flex justify-content-center">
+            <button class="nav_link logout fs-3 register_link d-flex align-items-center" type="submit"><i class="bi bi-box-arrow-left"></i><div class="register-container position-relative d-flex align-items-center overflow-hidden"><div class="register_link2 fs-5">logout</div></div></button>
+            <div class="link_hover"></div>
+        </div>
     </form>
-    <a class="nav_link" href="{{route('profile',['user' => Auth::user()])}}">{{Auth::user()->name}}</a>
+    <a class="nav_link d-flex" href="{{route('profile',['user' => Auth::user()])}}"><div class="nav_profile_icon"></div>{{Auth::user()->name}}</a>
         @endauth
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/vendi',[ArticleController::class,'create'])->name('vendi')->middlew
 Route::post('/store',[ArticleController::class,'store'])->name('store');
 Route::get('/detail/{article}',[ArticleController::class,'index'])->name('detail');
 Route::get('/profile/{user}',[ProfileController::class,'profile'])->name('profile');
-Route::post('/update/{user}',[ProfileController::class,'edit'])->name('user_update');
+Route::post('/update/{user}',[ProfileController::class,'update'])->name('user_update');
 Route::get('/profile_detail/{article}',[ArticleController::class,'profile_detail'])->name('profile_detail');
 Route::get('/article_update/{article}',[ArticleController::class,'edit'])->name('article_update');
 Route::put('/update/{article}',[ArticleController::class,'update'])->name('update');
